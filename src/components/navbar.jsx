e80fded85947ae9d25bd67881ae05fb9d8a4a7dc
Elebor Henry <henryekene8@gmail.com>
2024-06-02 07:00:06 -0700

    Feat: Packages Table

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import Hamburger from "../assets/icons/hamburger";
+import { useState } from "react";
+import Close from "../assets/icons/cloase";
 
 function Navbar() {
   const location = useLocation();
+  const [showMobile, setshowMobile] = useState(false);
+
+  const removeNavOverlay = () => {
+    setshowMobile(false);
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -10,7 +17,14 @@ function Navbar() {
         <Link to="/">
           <p className="text-white">Logo</p>
         </Link>
-        <Hamburger />
+        <div
+          onClick={() => {
+            setshowMobile(true);
+          }}
+          className="cursor-pointer"
+        >
+          <Hamburger />
+        </div>
       </div>
 
       <div className="bg-custom-black fixed z-20 md:w-[100vw] max-w-[1500px] md:flex justify-between items-center p-[16px] hidden">
@@ -61,6 +75,50 @@ function Navbar() {
         </div>
 
         <div className="flex w-[150px] justify-between items-center">
+          <p className="text-white">Login</p>
+          <p className="text-white bg-custom-green px-[16px] py-[4px] rounded-[20px]">
+            Sign Up
+          </p>
+        </div>
+      </div>
+
+      <div
+        className={
+          showMobile
+            ? `bg-custom-black z-20 nav landing2 h-[100vh] top-0 w-[100vw] flex flex-col items-center justify-center ml-[-1rem] fixed show`
+            : "bg-[#000009] h-[100vh] nav no top-0 w-[100vw] flex flex-col items-center justify-center fixed"
+        }
+      >
+        <div
+          className="absolute md:text-[2vw] headingfont text-white top-6 nav text-[4vw] left-[85%]"
+          onClick={() => {
+            setshowMobile(false);
+          }}
+        >
+          <Close />
+        </div>
+        <Link to="/" className="nav" onClick={removeNavOverlay}>
+          <p className="text-[4vw] md:text-[2vw] text-white mb-[1rem] headingfont font-bold">
+            Home
+          </p>
+        </Link>
+
+        <Link className="nav" to="/about" onClick={removeNavOverlay}>
+          <p className="text-[4vw] md:text-[2vw] text-white mb-[1rem] headingfont font-bold">
+            About
+          </p>
+        </Link>
+        <Link className="nav" to="/packages" onClick={removeNavOverlay}>
+          <p className="text-[4vw] md:text-[2vw] text-white mb-[1rem] headingfont font-bold">
+            Packages
+          </p>
+        </Link>
+        <Link className="nav" to="/contact" onClick={removeNavOverlay}>
+          <p className="text-[4vw] md:text-[2vw] text-white mb-[1rem] headingfont font-bold">
+            Contact
+          </p>
+        </Link>
+        <div className="flex w-[150px] mt-[24px] justify-between items-center">
           <p className="text-white">Login</p>
           <p className="text-white bg-custom-green px-[16px] py-[4px] rounded-[20px]">
             Sign Up
